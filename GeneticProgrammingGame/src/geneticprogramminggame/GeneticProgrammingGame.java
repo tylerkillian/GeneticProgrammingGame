ca8c35e8b751c918d9e22965985e3c7ff5599d27
tylerkillian <tylerkillian@tylers-mbp>
2016-03-23 22:06:20 -0400

    Main

--- GeneticProgrammingGame/src/geneticprogramminggame/GeneticProgrammingGame.java
+++ GeneticProgrammingGame/src/geneticprogramminggame/GeneticProgrammingGame.java
@@ -14,18 +14,6 @@ import java.awt.*;
  */
 public class GeneticProgrammingGame {
 
-    private static void createAndShowGUI() {
-        JFrame frame = new JFrame("MAIN");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.add(new GameView());
-
-        frame.pack();
-        //frame.setSize(800, 600);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-    }
-
     /**
      * @param args the command line arguments
      */
